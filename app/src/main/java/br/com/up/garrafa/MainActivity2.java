759a3af7d44c3f29088551130ac4d47095b61bae
lewycaron <lewycaron@gmail.com>
2022-09-28 03:40:43 -0300

    coletando infos de angulos

--- app/src/main/java/br/com/up/garrafa/MainActivity2.java
+++ app/src/main/java/br/com/up/garrafa/MainActivity2.java
@@ -14,7 +14,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     private ImageView roleta;
     private Random random = new Random();
-    private int ultimadirecao;
+    private double ultimadirecao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +24,28 @@ public class MainActivity2 extends AppCompatActivity {
 
         roleta = findViewById(R.id.idRoleta);
         roleta.setOnClickListener(view -> {
-
-
-
-            int i = 2880;
-            int direction = random.nextInt(i);
-            float eixoX = roleta.getWidth() /2;
-            float eixoY = roleta.getHeight() /2;
-
-            Animation virar = new RotateAnimation(ultimadirecao, direction, eixoX, eixoY);
-            virar.setDuration(2500);
-            virar.setFillAfter(true);
-
-            ultimadirecao = direction;
-            roleta.startAnimation(virar);
-
-
+            startRoleta();
         });
 
+    }
+
+    public void startRoleta()
+    {
+        int i = 360 * 8;
+        double direction = random.nextInt(i);
+        float eixoX = roleta.getWidth() /2;
+        float eixoY = roleta.getHeight() /2;
+
+        Animation virar = new RotateAnimation(0, (int)direction, eixoX, eixoY);
+
+        virar.setDuration(2500);
+        virar.setFillAfter(true);
+        // TODO: Descobrir o numero de voltas e a sobra multiplica por 360 vai achar a angulação.
+
+        double numeroVoltas =  direction / 360;
+        ultimadirecao = direction;
+        double valor_decimal = numeroVoltas - (int)(numeroVoltas);
+        double angulo = valor_decimal * 360;
+        roleta.startAnimation(virar);
     }
 }
